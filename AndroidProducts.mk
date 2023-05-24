b26b8f4dad136da8be604f776989310de18f7a7b
essaNAJJAR <51336462+essaNAJJAR@users.noreply.github.com>
2023-05-24 13:01:49 +0300

    sweet: Evolution-x Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,5 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+
+    $(LOCAL_DIR)/evolution_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    evolution_sweet-user \
+    evolution_sweet-userdebug \
+    evolution_sweet-eng
